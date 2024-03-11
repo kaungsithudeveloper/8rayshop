d9505bc0c8d3c2f1102c6d0572a8f9bd1a3dadae
kaungsithu <122274867+kaungsithudeveloper@users.noreply.github.com>
2024-03-11 20:32:50 +0630

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ return new class extends Migration
             $table->string('photo')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('aboutme')->nullable();
+            $table->text('address')->nullable();
+            $table->enum('role',['admin','emplopy','user'])->default('user');
+            $table->enum('status',['active','inactive'])->default('active');
             $table->rememberToken();
             $table->timestamps();
         });
